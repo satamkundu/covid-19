75566791f80e0b5d05f91a4712a51a8006b5a47e
Satam Kundu <satamkundu67@gmail.com>
2020-07-13 17:52:49 +0530

    Updates

---
+++ precache-manifest.eacd0fb2effd611595a20a6a1d9ac976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74be1fa607b6a1d10cb887180e1535d",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "831fe913c56643b7b3e4",
+    "url": "/covid-19/static/css/main.2674b84f.chunk.css"
+  },
+  {
+    "revision": "619a1b32381d171428a9",
+    "url": "/covid-19/static/js/2.4c586a41.chunk.js"
+  },
+  {
+    "revision": "995084f4dd276fa9cc26db82da00db86",
+    "url": "/covid-19/static/js/2.4c586a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831fe913c56643b7b3e4",
+    "url": "/covid-19/static/js/main.5c8e129a.chunk.js"
+  },
+  {
+    "revision": "138cfe9f8f539b769bc2",
+    "url": "/covid-19/static/js/runtime-main.c8a52902.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
